d2310d9e1ce8bb1b4abde0cad0c68d4ba291ea35
Radu Simionescu <radu.silviu.simionescu@gmail.com>
2017-01-21 21:19:48 +0200

    Adding initial project stub

---
+++ uRNDR/template/template.cpp
@@ -0,0 +1,15 @@
+/*
+    Creation Date   21 Jan 2017
+    Author          Radu
+    Description
+        Template cpp file for all new files in project
+*/
+
+// Class header file
+#include "template.h"
+
+// Project includes & 3rd party libs
+#include "timer.h"
+
+// std library includes
+#include <iostream>
---
+++ uRNDR/template/template.h
@@ -0,0 +1,26 @@
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+// project includes & 3rd party
+#include "spi.h"
+
+// std library includes
+#include <iostream>
+
+// Constants
+#define MAX_LENGTH 123U
+
+// Type definitions
+typedef struct
+{
+}
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __TEMPLATE_H__
